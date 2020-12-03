6410e79ffcf17d291e983c5a807949e6951219cb
Alexander Bocharov <alex.bocharov@outlook.com>
2020-12-03 21:58:36 +0300

    Fix renaming

--- src/pcs_mq_sdk_core.sql
+++ src/pcs_mq_sdk_core.sql
@@ -1,7 +1,7 @@
-create or replace package hcs_mq_sdk_core
+create or replace package pcs_mq_sdk_core
 as
   procedure send(p_url_base in varchar2, p_method in varchar, p_content_type in varchar2, p_content in varchar2, p_url_params in varchar2 default null);
-end hcs_mq_sdk_core;
+end pcs_mq_sdk_core;
 /
 
-show errors package hcs_mq_sdk_core;
+show errors package pcs_mq_sdk_core;
