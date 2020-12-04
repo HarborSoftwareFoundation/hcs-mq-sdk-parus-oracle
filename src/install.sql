84419d7d2fd14f2e5160026c88cd4900295b22fd
Alexander Bocharov <alex.bocharov@outlook.com>
2020-12-04 16:33:44 +0300

    Fix install.sql

--- src/install.sql
+++ src/install.sql
@@ -1,6 +1,6 @@
-@@hcs_mq_sdk_core.sql
-@@hcs_mq_sdk_core_body.sql
-@@hcs_mq_sdk_core$pg.sql
-@@hcs_mq_sdk_core$pg_body.sql
-@@hcs_mq_sdk.sql
-@@hcs_mq_sdk_body.sql
+@@pcs_mq_sdk_core.sql
+@@pcs_mq_sdk_core_body.sql
+@@pcs_mq_sdk_core$pg.sql
+@@pcs_mq_sdk_core$pg_body.sql
+@@pcs_mq_sdk.sql
+@@pcs_mq_sdk_body.sql
